f36a6f1d2f86c4349b9b3f380b4edf86e08a44b0
Anurag Basuri <145131978+Anurag-Basuri@users.noreply.github.com>
2026-06-27 15:59:15 +0530

    Add Disjoint Set Union implementation

---
+++ Disjoint_Set_Union/byRank.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DisjointSet {
+    vector<int> parent;
+    vector<int> rank;
+public:
+    DisjointSet(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (int i = 0; i < n; i++) 
+            parent[i] = i;
+    }
+
+    int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void unite(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if(rootX == rootY) return;
+        
+        if (rootX != rootY) {
+            if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+};
+
+int main() {
+    DisjointSet ds(10);
+
+    ds.unite(1, 2);
+    ds.unite(2, 3);
+    ds.unite(4, 5);
+    ds.unite(3, 4);
+    ds.unite(5, 6);
+
+    cout << "Are 1 and 3 connected? " << (ds.find(1) == ds.find(3) ? "Yes" : "No") << endl;
+    cout << "Are 1 and 4 connected? " << (ds.find(1) == ds.find(4) ? "Yes" : "No") << endl;
+    cout << "Are 5 and 6 connected? " << (ds.find(5) == ds.find(6) ? "Yes" : "No") << endl;
+
+    return 0;
+}
